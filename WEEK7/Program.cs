10211fbaedc08d4076d4868e6721834cc6c917bc
Ramandeep kaur <maanraman058@gmail.com>
2019-02-21 14:56:41 -0500

    commit 2.50

--- WEEK7/Program.cs
+++ WEEK7/Program.cs
@@ -27,7 +27,7 @@ namespace WEEK7
             Console.WriteLine(number);
             if (number == 0) { return 1; }
             return number * GetFactorial(number - 1);
-        }
+        } 
 
     }
 
